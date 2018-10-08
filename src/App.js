c6febe0c0ce347fe9d09171dadeeec1156fce289
Steve Castle <steve@tigerhaunt.com>
2018-10-07 19:32:34 -0700

    add dynamic import example without React.lazy

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import { createCache, createResource } from 'simple-cache-provider';
 import { Img } from './Img';
 const cache = createCache();
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// Lazy load!
+const getThing = createResource(
+  () => sleep(1000).then(() => import('./Thing').then(mod => mod.default)),
+  thing => thing
+);
+
+const Thing = props => {
+  const Comp = getThing.read(cache, props);
+  return <Comp {...props} />;
+};
+
 export default () => {
   return (
     <Fragment>
       <h1>Hello Future</h1>
+      <React.Placeholder delayMs={500} fallback={<div>🌀 'Loading....'</div>}>
+        <Thing />
+      </React.Placeholder>
       <React.Placeholder delayMs={500} fallback={<div>🌀 'Loading....'</div>}>
         <Img
           cache={cache}
